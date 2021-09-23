086fe549e1025c00f326eb78275d563de5795630
Lawrence Splaver <lsplaver@yahoo.com>
2021-09-23 15:33:33 -0400

    Initial seed data and database models coded

---
+++ ContactManager/Models/Contact.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContactManager.Models
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+
+        [Required(ErrorMessage = "Please enter a first name.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter a last name.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter a phone number.")]
+        [Phone(ErrorMessage = "Not a valid phone number.")]
+        public /*PhoneAttribute*/ string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please enter an email address.")]
+        [EmailAddress(ErrorMessage = "Not a valid email address.")]
+        public /*EmailAddressAttribute*/ string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter a category.")]
+        public int? CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public string Organization { get; set; }
+
+        public DateTime DateCreated { get; set; }
+    }
+}
---
+++ ContactManager/Models/ContactContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactManager.Models
+{
+    public class ContactContext : DbContext
+    {
+        public ContactContext(DbContextOptions<ContactContext> options) : base(options)
+        { }
+
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contact>().HasData(
+                new Contact
+                {
+                    ContactId = 1,
+                    FirstName = "John",
+                    LastName = "Doe",
+                    Phone = "541-534-4923",
+                    Email = "john.doe@yahoo.com",
+                    CategoryId = 1,
+                    Organization = ""
+                },
+                new Contact
+                {
+                    ContactId = 2,
+                    FirstName = "Jane",
+                    LastName = "Smith",
+                    Phone = "316-421-4521",
+                    Email = "jsmith@gmail.com",
+                    CategoryId = 2,
+                    Organization = ""
+                }
+            );
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = 1, Name = "Friend"},
+                new Category { CategoryId = 2, Name = "Coworker"},
+                new Category { CategoryId = 3, Name = "Family"},
+                new Category { CategoryId = 4, Name = "Medical" },
+                new Category { CategoryId = 5, Name = "Other"}
+            );
+        }
+    }
+}
