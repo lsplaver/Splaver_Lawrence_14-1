7534e02f4a4b9c2c670b04754f1450aa1e3eaf68
Lawrence Splaver <lsplaver@yahoo.com>
2021-09-23 21:38:22 -0400

    HomeController.cs coded

--- ContactManager/Controllers/HomeController.cs
+++ ContactManager/Controllers/HomeController.cs
@@ -1,15 +1,26 @@
-﻿using ContactManager.Models;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ContactManager.Models;
 
 namespace ContactManager.Controllers
 {
     public class HomeController : Controller
     {
+        private ContactContext context { get; set; }
+
+        public HomeController(ContactContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            var contacts = context.Contacts
+                .Include(c => c.Category)
+                .OrderBy(c => c.LastName)
+                .ToList();
+            return View(contacts);
+        }
     }
 }
