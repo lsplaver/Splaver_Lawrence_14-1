72801ee0dfb959128f59344c528d50b2d98ee58f
Lawrence Splaver <lsplaver@yahoo.com>
2021-12-01 13:46:25 -0500

    Changed namespace for CustomEmailFormatAttrinute and CustomPhoneFormatAttribute.  Updated using for Contact.  Updated SQLSwer and SQLTools nuget packages to lastest 3.x.

--- ContactManager/Models/DomainModels/Contact.cs
+++ ContactManager/Models/DomainModels/Contact.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
-using ContactManager.Models.Validation;
 
 namespace ContactManager.Models
 {
--- ContactManager/Models/Validation/CustomEmailFormatAttribute.cs
+++ ContactManager/Models/Validation/CustomEmailFormatAttribute.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace ContactManager.Models.Validation
+namespace ContactManager.Models
 {
     public class CustomEmailFormatAttribute : ValidationAttribute
     {
